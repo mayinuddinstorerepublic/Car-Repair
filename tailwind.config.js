53ea82633b560bd573f6fc2fd08d71f7d9bd974d
Mayinuddin <mayinuddin.storerepublic@gmail.com>
2024-01-24 11:46:42 +0600

    typography setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false,
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
